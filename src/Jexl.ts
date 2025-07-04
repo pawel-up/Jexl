21e96bcad6deff29be28052cd6ee5b64d47ce74f
Pawel Uchida-Psztyc <jarrodek@gmail.com>
2025-07-04 11:41:45 -0700

    feat: adding main index file
    Signed-off-by: Pawel Uchida-Psztyc <jarrodek@gmail.com>

--- src/Jexl.ts
+++ src/Jexl.ts
@@ -619,30 +619,3 @@ export class Jexl {
     this.grammar.elements[str] = obj
   }
 }
-
-// Export Validator and related types for advanced use cases
-export { Validator } from './Validator.js'
-export type { ValidationResult, ValidationIssue, ValidationOptions, ValidationSeverity } from './Validator.js'
-export type {
-  FunctionFunction,
-  ComparableValue,
-  BaseASTNode,
-  ArrayLiteralNode,
-  BinaryExpressionNode,
-  ConditionalExpressionNode,
-  FilterExpressionNode,
-  IdentifierNode,
-  LiteralNode,
-  ObjectLiteralNode,
-  UnaryExpressionNode,
-  ASTNode,
-  GrammarElement,
-  BinaryElement,
-  UnaryElement,
-  Grammar,
-  FunctionCallNode,
-  TransformFunction,
-  BinaryOpFunction,
-  UnaryOpFunction,
-  EvaluationContext,
-} from './grammar.js'
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+export type {
+  FunctionFunction,
+  ComparableValue,
+  BaseASTNode,
+  ArrayLiteralNode,
+  BinaryExpressionNode,
+  ConditionalExpressionNode,
+  FilterExpressionNode,
+  IdentifierNode,
+  LiteralNode,
+  ObjectLiteralNode,
+  UnaryExpressionNode,
+  ASTNode,
+  GrammarElement,
+  BinaryElement,
+  UnaryElement,
+  Grammar,
+  FunctionCallNode,
+  TransformFunction,
+  BinaryOpFunction,
+  UnaryOpFunction,
+  EvaluationContext,
+} from './grammar.js'
+export { Validator } from './Validator.js'
+export { type ValidationResult, type ValidationIssue, type ValidationOptions, ValidationSeverity } from './Validator.js'
+export { Jexl } from './Jexl.js'
